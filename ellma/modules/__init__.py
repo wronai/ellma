932ee7631121d1d73d3652b1c80ff59322756900
tom-sapletta-com <tom@sapletta.com>
2025-06-07 17:52:14 +0200

    update

--- ellma/modules/__init__.py
+++ ellma/modules/__init__.py
@@ -5,9 +5,19 @@ This module handles dynamic loading and management of ELLMa modules.
 """
 
 from ellma.modules.registry import ModuleRegistry, ModuleMetadata, get_registry
+from ellma.modules.generated_utils import (
+    enhanced_error_handler,
+    PerformanceCache,
+    parallel_map,
+    default_cache
+)
 
 __all__ = [
     'ModuleRegistry',
     'ModuleMetadata',
-    'get_registry'
+    'get_registry',
+    'enhanced_error_handler',
+    'PerformanceCache',
+    'parallel_map',
+    'default_cache'
 ]
